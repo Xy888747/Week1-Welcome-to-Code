8768f9a956f7137e01710a4eeffb8ff0c65c2a64
X <abankfris@gmail.com>
2025-03-24 12:29:41 +0700

    soal1.js

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,16 @@
+// ## Soal 3. Breaking Sentence (Again) using Substring
+// ```js
+// // Problem
+// // Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+let secondword = word3.substring(4, 14);
+let thirdword = word3.substring(15, 17)
+let fourthword = word3.substring(18, 20)
+let fifthword = word3.substring(21, 25)
+console.log('First Word: ' + exampleFirstWord3);
+console.log('second word: ' + secondword);
+console.log('third word: ' + thirdword);
+console.log('fourth word: ' + fourthword);
+console.log('fifth word: ' + fifthword);
---
+++ quiz/part2/soal4.js
@@ -0,0 +1,28 @@
+
+// ## Soal 4. Breaking Sentence (yet Again) and Count Each Length
+// ```js
+// // Problem
+// // Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3);
+let secondword = word4.substring(4, 14);
+let thirdword = word4.substring(15, 17)
+let fourthword = word4.substring(18, 20)
+let fifthword = word4.substring(21, 25)
+
+let firstWordLength = exampleFirstWord4.length;
+let secondwordLength = secondword.length;
+let thirdwordLength = thirdword.length;
+let fourthwordLength = fourthword.length;
+let fifthwordLength = fifthword.length;
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+console.log('Second word: ' + secondword + ', with length: ' + secondwordLength);
+console.log('third word: ' + thirdword + ', with length: ' + thirdwordLength);
+console.log('fouth word: ' + fourthword + ', with length: ' + fourthwordLength);
+console.log('fifth word: ' + fifthword + ', with length: ' + fifthwordLength);
+// tulis code sesuai dengan keterangan soalnya dan pola example yang ada
+
+
+// Selamat Mengerjakan 👨🏻‍🌾
