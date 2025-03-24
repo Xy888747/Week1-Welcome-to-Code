eab2efde579cef95798bfe2f09585f93856b7950
X <abankfris@gmail.com>
2025-03-24 13:04:56 +0700

    soal1.js

---
+++ quiz/part3/soal5.js
@@ -0,0 +1,17 @@
+// ## Soal 5. Bintang asteriks
+// ```js
+// // Problem buatlah bintang seperti berikut
+// let input = 5
+// //hasilnya
+// //*
+// //**
+// //***
+// //****
+// //*****
+// ```
+
+// ---
+// Tips pengerjaan ada di video berikut :
+// [Looping](https://youtu.be/kyobpgoqx2c)
+
+// [Materi Looping](../../study-materials/part5.md)
