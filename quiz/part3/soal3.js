558fbaeda1f5e2b21edfe7b1bf983267751fc608
X <abankfris@gmail.com>
2025-03-26 07:34:09 +0700

    soal3.js

--- quiz/part3/soal3.js
+++ quiz/part3/soal3.js
@@ -1,11 +1,22 @@
 // ## Soal 3. Angka Ganjil dan Genap
 
-// ```js
-// // Hint: kamu akan menggunakan kondisional juga di kasus ini.
 
-// // Problem
-// // Buatlah sebuah perulangan 1 - 100 dengan pertambahan counter sebanyak 1
-// // Di dalam perulangan, periksa setiap angka counter:
+// Hint: kamu akan menggunakan kondisional juga di kasus ini.
 
-// // Apabila angka counter adalah angka genap, tuliskan GENAP
-// // Apabila angka counter adalah angka ganjil, tuliskan GANJIL
+// Problem
+// Buatlah sebuah perulangan 1 - 100 dengan pertambahan counter sebanyak 1
+// Di dalam perulangan, periksa setiap angka counter:
+
+// Apabila angka counter adalah angka genap, tuliskan GENAP
+// Apabila angka counter adalah angka ganjil, tuliskan GANJIL
+
+
+let i = 0;
+while ( i < 100 )  { i++;
+    if (i === 0) continue;
+    else if (i % 2 === 0 ) {
+        console.log("Genap =", (i))
+    } else if (i %2 !== 0) {
+        console.log("Ganjil=", (i))
+    }
+} 
