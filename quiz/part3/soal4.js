6970266923188935668a22622d0a41a5b5d386c8
X <abankfris@gmail.com>
2025-03-28 07:48:08 +0700

    soal4.js

--- quiz/part3/soal4.js
+++ quiz/part3/soal4.js
@@ -6,3 +6,27 @@
 // Apabila bukan kelipatan yang ditentukan tidak perlu menuliskan apa-apa
 // Apabila angka counter adalah kelipatan 3 dengan pertambahan 2, kelipatan 6 dengan pertambahan 5, dan kelipatan 10 dengan pertambahan 9, tuliskan:
 // "3 kelipatan 3"dan seterusnya.
+
+// 
+
+
+for (let i = 1; i < 100; i+= 2 ) {
+    if (i % 3 === 0) { 
+    console.log(`${i}: 3 kelipatan 3`)
+    }
+}
+
+//
+
+for (let j = 1; j < 100; j+= 5) {
+    if (j % 6 === 0) {
+        console.log(`${j}: 6 kelipatan 6`)
+   }
+}
+
+for (let k = 1; k < 100; k+= 9) {
+    if (k % 10 === 0) {
+        console.log(`${k}: 10 kelipatan 10`)
+    }
+}
+
